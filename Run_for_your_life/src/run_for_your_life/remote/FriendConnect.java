04597ee84f37fa0e7e92d531261a94f248d6e134
Kory Stiger <kstiges@gmail.com>
2014-04-17 22:53:16 -0400

    Fucked up the last merge, fixed that, should be good now.

--- Run_for_your_life/src/run_for_your_life/remote/FriendConnect.java
+++ Run_for_your_life/src/run_for_your_life/remote/FriendConnect.java
@@ -1,13 +1,8 @@
 package run_for_your_life.remote;
 
-<<<<<<< HEAD
 import run_for_your_life.user.User;
 
 public interface FriendConnect{
-=======
-public static interface FriendConnect{
->>>>>>> branch 'master' of https://github.com/edwardwsears/Run-For-Your-Life.git
-
     public String[] searchFriends(String name);
 
     public User getFriendInfo(String username);
--- Run_for_your_life/src/run_for_your_life/remote/OpponentSearch.java
+++ Run_for_your_life/src/run_for_your_life/remote/OpponentSearch.java
@@ -1,13 +1,8 @@
 package run_for_your_life.remote;
 
-<<<<<<< HEAD
 import run_for_your_life.user.User;
 
 public interface OpponentSearch{
-=======
-public static interface OpponentSearch{
->>>>>>> branch 'master' of https://github.com/edwardwsears/Run-For-Your-Life.git
-
     public boolean isOnline(String name);
 
     public User[] getAvailableOpponents();
